467c7f693d9d3a32a8c3928322323c52d42540f1
dot-cera <ritaclaris980@gmail.com>
2024-01-09 11:26:56 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, driver){
+    const theDriver = driver.toUpperCase();
+    const matchingDrivers = array.filter(element => element.toUpperCase() === theDriver)
+    return matchingDrivers;
+}
+
+function fuzzyMatch(array, begginingLetters){
+    const matchingDrivers = array.filter(element => element.startsWith(begginingLetters));
+    return matchingDrivers;
+}
+
+function matchName(array, driver){
+    const matchingDrivers = array.filter(element => element.name === driver) 
+    return matchingDrivers;
+}
